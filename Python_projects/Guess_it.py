d149bec7ea054efb2a048bc33fc0acf9985b7dea
Chlocode <chalafeghaquakumey@gmail.com>
2023-05-10 18:57:00 +0000

    Update Guess_it.py
    Edited print messages

--- Python_projects/Guess_it.py
+++ Python_projects/Guess_it.py
@@ -1,5 +1,6 @@
 import random
-"""This program does sth. not sure of it yet hehe
+"""This is a guessing game.
+The user tries to guess a number between 1 and 10 which the computer has chosen
 """
 def main():
     pimil_beonho = random.randrange(1, 10)
@@ -8,12 +9,12 @@ def main():
     while guess != pimil_beonho:
         guess = float(input("Guess a number between 1 and 10: "))
         if(guess < pimil_beonho and guess > 0):
-            print("Kuku dig ground eneter as you wanna show us how low you can go")
+            print("You could dig the ground and jump in as you'd love to show us how low you can go. Try again")
         elif (guess > pimil_beonho and guess < 10):
-            print("Relax, if you aimed as high with your dreams, you wouldn't be playing game and failing like this. Try again")
+            print("Whoa there buddy, aim high with your dreams, not this game. Try again")
         elif (guess > 10 or guess < 0):
             print(f"*bombastic side eye*")
-    print("Congrats. Now go do sth right with your life.")
+    print("Congrats. Now, go do something productive.")
 
 if __name__ == "__main__":
     main()
